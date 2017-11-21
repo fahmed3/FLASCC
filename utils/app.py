93e8a8bb1e39598842755743eba42f2321f83e90
Stanley Lin <slin55055@gmail.com>
2017-11-21 14:16:19 -0500

    added empty app.py

---
+++ utils/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.secret_key = os.urandom(256)
+
+@app.route("/")
+def root():
+	return "hello!"
+
+if __name__ = "__main__":
+	app.debug = True
+	app.run()
+
