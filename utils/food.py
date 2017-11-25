8d27464151c8d5689eb201a8fe7e91804a9c8cff
Stanley Lin <slin55055@gmail.com>
2017-11-25 13:33:06 -0500

    forgot to include this

---
+++ utils/food.py
@@ -0,0 +1,33 @@
+import requests
+
+def get_restaurants(key, origin, keyword):
+	pass
+
+def get_restaurant(key, restaurant_id):
+	pass
+
+#can't use dict because its a reserved keyword
+def get_rating(d):
+	pass
+
+def get_address(d):
+	pass
+
+def get_menu(d):
+	pass
+
+def get_cuisines(d):
+	pass
+
+def get_name(d):
+	pass
+
+def get_num_of_reviews(d):
+	pass
+
+def get_id(d):
+	'''
+	for use when passing data from results page to info page
+	'''
+	pass
+
